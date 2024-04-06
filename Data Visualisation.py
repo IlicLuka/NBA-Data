b1454d849a5e79abddf90f1b2c4393ce465ac772
Luka Ilic <lukvin@hotmail.com>
2024-04-06 16:11:35 +0100

    feat: Plot ready data + statistical summaries

--- Data Visualisation.py
+++ Data Visualisation.py
@@ -7,21 +7,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
 
-df = pd.read_csv("C:\\Users\\lukvi\\Python Fun\\Data Visualisation\\NBA Data\\triple_doubles.csv")
-df["season"] = df["season"].astype("string").transform(lambda x: x[-2:])
-df_stats = df[["MIN","PTS","REB","AST","STL","BLK"]]
-# print(df.head())
+td = pd.read_csv("C:\\Users\\lukvi\\Python Fun\\NBA-Data\\triple_doubles.csv")
+td["season"] = td["season"].astype("string").transform(lambda x: x[-2:])
+td_stats = td.groupby("player",as_index=True,sort=False).apply(lambda x: x.describe())[["MIN","PTS","REB","AST","STL","BLK"]]
+print(td_stats.head())
 
 # TD leaders
-df_counts = df.groupby("player",as_index=False).apply(lambda x: x)["player"].value_counts()
-#df_leaders = df.loc[df_counts[0,:5]]
-print(df_counts.head())
-
-df_print = df.groupby("season",as_index=True,sort=False).apply(lambda x: x.count())
-df_stats_print = df.groupby("player",as_index=True,sort=False).apply(lambda x: x.describe())[["MIN","PTS","REB","AST","STL","BLK"]]
-# df_stats_leaders_summary = df_leaders.groupby("player",as_index=True,sort=False).apply(lambda x: x.describe())[["MIN","PTS","REB","AST","STL","BLK"]]
-print(df_stats_print)
+td_counts = td["player"].value_counts()
+td_top_5 = td_stats.loc[td_counts.index[0:5].to_list()]
+print(td_top_5.index)
 
 # sns.pairplot(df_stats)
 # plt.show()
-
