b8f1b281b734cde894409f3a3929ecaba9254daa
Luka Ilic <lukvin@hotmail.com>
2024-04-08 21:24:17 +0100

    feat: First Plots

--- Data Visualisation.py
+++ Data Visualisation.py
@@ -7,15 +7,43 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
 
-td = pd.read_csv("C:\\Users\\lukvi\\Python Fun\\NBA-Data\\triple_doubles.csv")
+# Loading the data
+td = pd.read_csv("C:\\Users\\lukvi\\Python Fun\\NBA-Data\\triple_doubles.csv") 
 td["season"] = td["season"].astype("string").transform(lambda x: x[-2:])
-td_stats = td.groupby("player",as_index=True,sort=False).apply(lambda x: x.describe())[["MIN","PTS","REB","AST","STL","BLK"]]
-print(td_stats.head())
 
-# TD leaders
+# The leaders' TDs
 td_counts = td["player"].value_counts()
-td_top_5 = td_stats.loc[td_counts.index[0:5].to_list()]
-print(td_top_5.index)
+td_leaders = td.groupby("player",as_index=True,sort=False).apply(lambda x: x)[["MIN","PTS","REB","AST","STL","BLK"]]
+td_leaders = td_leaders.loc[td_counts.index[0:5].to_list()]
+td_leaders = td_leaders.reset_index().drop("level_1",axis=1)
 
-# sns.pairplot(df_stats)
+# TD stats for all TDs
+td_stats = td.groupby("player",as_index=True,sort=False).apply(lambda x: x.describe())[["MIN","PTS","REB","AST","STL","BLK"]]
+td_means = td_stats.reset_index()[td_stats.reset_index()["level_1"] == "mean"]
+
+# TD leaders' means
+td_top_5 = td_stats.loc[td_counts.index[0:5].to_list()]
+td_top_5_means = td_top_5.reset_index()[td_top_5.reset_index()["level_1"]=="mean"]
+
+
+# Boxplots comparing the means of all players TD stats
+# sns.catplot(td_means,kind="box")
 # plt.show()
+
+# Boxplots comparing stats by person
+# westbrook_td = td_leaders[td_leaders["player"]=="Russell Westbrook"]
+# sns.catplot(westbrook_td,kind="violin")
+# plt.show()
+
+# Boxplot comparing players by category
+leaders_pts = td.groupby("player",as_index=True,sort=False).apply(lambda x: x)["PTS"]
+print(leaders_pts.T)
+# leaders_pts = None
+# sns.catplot(leaders_pts,kind="box")
+# plt.show()
+
+# TODO: Comperative Boxplots
+# g = sns.FacetGrid(td_leaders)
+# g.map(sns.boxplot,"player","PTS")
+# g.add_legend()
+# plt.show
